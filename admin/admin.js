2b850f1beb14d0fd620b2b056357af1bf742ec7d
xarn <arnaud.dagnelies@gmail.com>
2016-10-30 09:13:50 +0000

    ...

--- admin/admin.js
+++ admin/admin.js
@@ -1,9 +1,86 @@
+/* global $ */
+/* global ace */
+
 var files_url = '/@files/'
 var views_url = '/@files/'
 var editor = null
+//var editor_container = $('#editor_container')
+var preview = null
 var tree = null
 
 var modelist = ace.require("ace/ext/modelist")
+
+var DEFAULT_HANDLER = {
+  "editor": false,
+  "preview": false
+}
+
+var handlers = {
+  "application": {
+    "editor": true,
+    "preview": false
+  },
+  "application/json": {
+    "editor": true,
+    "preview": false
+  },
+  "application/xml": {
+    "editor": true,
+    "preview": false
+  },
+  "application/pdf": {
+    "editor": false,
+    "preview": true
+  },
+  "audio": {
+    "editor": false,
+    "preview": true
+  },
+  "example": {
+    "editor": false,
+    "preview": false
+  },
+  "image": {
+    "editor": false,
+    "preview": true
+  },
+  "message": {
+    "editor": false,
+    "preview": true
+  },
+  "multipart": {
+    "editor": false,
+    "preview": false
+  },
+  "multipart": {
+    "editor": false,
+    "preview": false
+  },
+  "text": {
+    "editor": true,
+    "preview": false
+  },
+  "text/html": {
+    "editor": true,
+    "preview": true
+  },
+  "video": {
+    "editor": false,
+    "preview": true
+  }
+}
+
+function getHandler(mime_type) {
+  mime_type = mime_type.split(';')[0]
+  if( mime_type in handlers )
+    return handlers[mime_type]
+    
+  mime_type = mime_type.split('/')[0]
+  if( mime_type in handlers )
+    return handlers[mime_type]
+  
+  return DEFAULT_HANDLER
+}
 
 function init() {
   tree = $('#filetree').jstree({
@@ -29,6 +106,7 @@ function init() {
 
   
   editor = ace.edit("editor");
+  preview = document.getElementById('preview')
   
   tree.on('changed.jstree', onSelect)
   
@@ -45,39 +123,67 @@ var binary = ['png']
 
 function onSelect(event, data) {
   console.debug(data)
-  editor.setValue('')
+  
+  hidePreview()
+  hideEditor()
   
   var path = data.node.id
-  var mode = modelist.getModeForPath(path).mode
-  var ext = getExt(path)
+  var url = '/@files/' + path
   
   
-  editor.session.setMode(mode)
-  
+  function onSuccess(res, a, xhr) {
+    console.debug(xhr)
+    showFile(url, xhr.getResponseHeader('content-type'), res)
+  }
+
   $.ajax({
-    url: '/@files/' + data.node.id,
+    url: url,
     converters: null,
     dataFilter: null,
     dataType: 'text',
-    success: updateEditor
+    success: onSuccess
   })
-  $('#view').prop('src', views_url + data.node.id)
-  //var modelist = ace.require("ace/ext/modelist")
-  //var mode = modelist.getModeForPath(data.node.text).mode
-  //editor.session.setMode(mode)
 }
 
-function updateEditor(res, a, xhr) {
-  console.debug(res)
-  console.debug(xhr)
-  var type = xhr.getResponseHeader('content-type')
-  console.debug(type)
-  if( type )
-    type = type.split('/')[0]
+function showFile(url, mime_type, data) {
+  console.debug(url)
+  console.debug(mime_type)
+  console.debug(text)
   
-  if( ['text', 'application', 'message'].indexOf(type) >= 0 ) {
-    editor.setValue(res, -1) // -1 to move the cursor at the start of file
-  }
+  var h = getHandler(mime_type)
+  
+  if( h.preview )
+    showPreview(url)
+  
+  if( h.editor )
+    showEditor(url, data)
+}
+
+function hidePreview() {
+  preview.hidden = true //.hide()
+}
+
+function showPreview(url) {
+  preview.hidden = false //.show()
+  preview.src = url //prop('src', url)
+}
+
+function hideEditor() {
+  editor.setValue('', -1)
+  //editor_container.hide()
+  //editor.container.style.display = "none" 
+  editor.container.hidden = true
+}
+
+function showEditor(path, content) {
+  
+  var mode = modelist.getModeForPath(path).mode
+  editor.session.setMode(mode)
+  
+  //editor.container.style.display = "block" //show()
+  editor.container.hidden = false
+  editor.setValue(content, -1) // -1 to move the cursor at the start of file
+  
 }
 
 /*
